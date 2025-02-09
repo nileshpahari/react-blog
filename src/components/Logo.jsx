c83d7d6f18f8da0b9039715f374e0ac2be23781d
Nilesh Pahari <nileshpahari@protonmail.com>
2025-02-10 04:00:00 +0530

    add styling to header and footer

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,10 +1,10 @@
 
 import React from 'react'
 
-function Logo({height="auto", width="100px"}) {
+function Logo({height="auto", width="100px", className=""}) {
   return (
     // <img src="../img/logo.png" alt="Blog logo" className={`h-[${height}] w-[${width}]`}/>
-    <div className={`h-[${height}] w-[${width}] font-bold`}>BLOG</div>
+    <div className={`h-[${height}] w-[${width}] font-bold ${className}`}>BLOG</div>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div>Login in to see contents</div>
   )
 }
 
