3e50b6d256f473a645549b130af87f0eccad009a
Nilesh Pahari <nileshpahari@protonmail.com>
2025-02-22 03:09:16 +0530

    add Post not found bug fix

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -20,17 +20,11 @@ function Post() {
       .then((post) => {
         if (post) {
           setPost(post);
-        } else {
-          return (
-            <div className="min-h-screen flex justify-center items-center">
-              <h1 className="text-3xl text-white">
-                Failed to find the post with slug: {slug}
-              </h1>
-            </div>
-          );
         }
       })
-      .catch((error) => console.log(error))
+      .catch((error) => {
+        console.log("Error while fetching the post ", error);
+      })
       .finally(() => setLoading(false));
   }, [slug, navigate]);
 
@@ -48,44 +42,56 @@ function Post() {
     }
   };
 
-  return loading ? (
-    <Loader />
-  ) : (
-    <div className="min-h-screen flex justify-center items-center py-10 px-4 ">
-      <div className="w-11/12  bg-gray-800 p-8 rounded-lg shadow-lg text-white ">
-        {post.featuredImage && (
-          <div className="mb-6">
-            <img
-              src={storageService.previewFile(post.featuredImage)}
-              alt="Post featured image"
-              className="w-full h-80 object-cover rounded-lg"
-            />
-          </div>
-        )}
-        <h1 className="text-4xl font-bold mb-4">{post.title}</h1>
-        <div className="prose prose-invert max-w-none mb-6">
-          {parse(post.content)}
+  if (loading) {
+    return <Loader />;
+  } else {
+    if (!post) {
+      return (
+        <div className="min-h-screen flex justify-center items-center">
+          <h1 className="text-3xl text-white">
+            Failed to find the post with slug: {slug}
+          </h1>
         </div>
+      );
+    } else {
+      return (
+        <div className="min-h-screen flex justify-center items-center py-10 px-4 ">
+          <div className="w-11/12  bg-gray-800 p-8 rounded-lg shadow-lg text-white ">
+            {post && post.featuredImage && (
+              <div className="mb-6">
+                <img
+                  src={storageService.previewFile(post.featuredImage)}
+                  alt="Post featured image"
+                  className="w-full h-80 object-cover rounded-lg"
+                />
+              </div>
+            )}
+            <h1 className="text-4xl font-bold mb-4">{post.title}</h1>
+            <div className="prose prose-invert max-w-none mb-6">
+              {parse(post.content)}
+            </div>
 
-        {isAuthor && (
-          <div className="flex  space-x-4 mt-32">
-            <Button
-              classname=" bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-lg "
-              onClick={() => navigate(`/edit-post/${post.$id}`)}
-            >
-              Edit
-            </Button>
-            <Button
-              classname=" bg-red-500 hover:bg-red-600 text-white font-semibold py-2 px-4 rounded-lg "
-              onClick={deletePost}
-            >
-              Delete
-            </Button>
+            {isAuthor && (
+              <div className="flex  space-x-4 mt-32">
+                <Button
+                  classname=" bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-lg "
+                  onClick={() => navigate(`/edit-post/${post.$id}`)}
+                >
+                  Edit
+                </Button>
+                <Button
+                  classname=" bg-red-500 hover:bg-red-600 text-white font-semibold py-2 px-4 rounded-lg "
+                  onClick={deletePost}
+                >
+                  Delete
+                </Button>
+              </div>
+            )}
           </div>
-        )}
-      </div>
-    </div>
-  );
+        </div>
+      );
+    }
+  }
 }
 
 export default Post;
